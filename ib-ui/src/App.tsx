48c26d2d4fb8b109eae7d2d9afb603c31bf146c2
Michael Lorton <malvoliosf@gmail.com>
2022-11-22 06:22:15 -0800

    default page

--- ib-ui/src/App.tsx
+++ ib-ui/src/App.tsx
@@ -1,33 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import PageFrame from "./PageFrame";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <PageFrame headline={`Welcome to Icebreaker.biz`}>
+      <div className="flex flex-col items-center justify-between">
+        <h3>Scan a badge to begin</h3>
       </div>
-      <h1>Icebreaker Quiz</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </PageFrame>
   );
 }
 
